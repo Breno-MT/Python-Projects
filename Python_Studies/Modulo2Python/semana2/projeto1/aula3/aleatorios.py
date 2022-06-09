1fc64b6794dfc0d416574705165cde311c4084a5
Breno <hentgenp@gmail.com>
2022-06-09 19:53:40 -0300

    Update aleatorios.py

---
+++ Python_Studies/Modulo2Python/semana2/projeto1/aula3/aleatorios.py
@@ -0,0 +1,22 @@
+import random
+
+random.seed(2) # inicia a semente para manter um padrão, dependendo da forma pode não haver mudanças
+
+
+print(random.randint(0,20))
+
+random_list = random.sample(range(10,30), 5)
+print("Random List")
+print(random_list)
+
+print('Aleatórios pares:')
+print(random.randrange(0,50,2))
+
+items = [1,2,3,4,5,6,7,8,9,10]
+print('Embaralhar items')
+random.shuffle(items)
+print(items)
+
+
+print("Escolhendo um item da lista de forma aleatória")
+print(random.choice(items))
