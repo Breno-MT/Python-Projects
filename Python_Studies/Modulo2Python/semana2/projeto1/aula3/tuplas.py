2326118c8b51fc9a074912ec30e46c11748a6a6a
Breno <hentgenp@gmail.com>
2022-06-09 19:46:35 -0300

    Update main > tuplas
    Para fazer coerencia com o que tem no arquivo.

---
+++ Python_Studies/Modulo2Python/semana2/projeto1/aula3/tuplas.py
@@ -0,0 +1,25 @@
+# from collections import namedtuple
+# Importanto uma biblioteca para criar uma tupla mais organizada
+
+# # tupla = (10,) # é considerada uma tupla por conta da virgula, se tivesse sem parenteses e com virgula ainda sim seria uma tupla
+
+# # print(type(tupla))
+
+# Estados = namedtuple('Estado', ['sigla', 'nome', 'regiao'])
+
+# estado_rj = Estados("RJ", "Rio de Janeiro", "Sudoeste")
+# estado_sp = Estados("SP", "São Paulo", "Sudeste")
+# estado_sc = Estados("SC", "Santa Catarina", "Sul")
+
+# print(estado_rj)
+# print(estado_sp)
+# print(estado_sc)
+
+
+# Tuplas com lista dentro
+# tupla = ([1,2,3], [4,5,6])
+
+# print(tupla)
+
+# tupla[0][1] = "oi"
+# print(tupla)
