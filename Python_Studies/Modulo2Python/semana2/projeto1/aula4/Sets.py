33a60e601cf7710926fd7396db80f9d422b18d75
Breno <hentgenp@gmail.com>
2022-06-10 19:23:40 -0300

    Update sets

---
+++ Python_Studies/Modulo2Python/semana2/projeto1/aula4/Sets.py
@@ -0,0 +1,20 @@
+set1 = {1,2,3}
+
+print(set1)
+
+# adiciona um elemento
+set1.add(6)
+set1.add(39)
+print(set1)
+
+# adiciona mais de 1 elemento
+set1.update([4,5,7])
+
+set_sorted = sorted(set1)
+
+print(set_sorted)
+
+# remove algum valor especifico do set, mas se não houver vai retornar erro
+set1.remove(1)
+print(set1)
+
