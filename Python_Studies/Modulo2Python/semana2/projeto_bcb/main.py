0132865b209e4a3089a8f81f7173e6d513877491
Breno <hentgenp@gmail.com>
2022-06-09 21:37:08 -0300

    Update main
    Colocado moedas currency atualmente.

--- Python_Studies/Modulo2Python/semana2/projeto_bcb/main.py
+++ Python_Studies/Modulo2Python/semana2/projeto_bcb/main.py
@@ -1,7 +1,9 @@
-from bcb import sgs
+from bcb import currency
 import matplotlib.pyplot as plt
 
-ipca = sgs.get(('IPCA', 433), last=12)
+# ipca = currency.get(('IPCA', 433), last=12)
+moedas = currency.get(['USD', 'EUR'], start='2000-01-01', end='2021-06-09', side='ask')
 
-plt.plot(ipca)
+# plt.plot(moedas)
+moedas.plot(figsize=(12, 6));
 plt.show()
