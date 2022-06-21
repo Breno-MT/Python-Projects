ff71854b9aaf152a0db3d5f48128485b45848725
Breno <hentgenp@gmail.com>
2022-06-21 20:22:41 -0300

    Create exercicio02.py

---
+++ Python_Studies/Modulo2Python/semana4/aula02/exercicio02.py
@@ -0,0 +1,18 @@
+import sys
+
+class Passagens:
+
+    def __init__(self):
+        pass
+
+    def comprarPassagem(self):
+        pass
+
+    def cancelarPassagem(self):
+        pass
+
+    def alterarPoltrona(self):
+        pass
+
+    def sair(self):
+        sys.exit()
