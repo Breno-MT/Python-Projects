2b7348686fa837b64c50fe1199a6c51c17ba18d8
Breno <hentgenp@gmail.com>
2022-06-29 22:10:26 -0300

    [UPDATE] classe_abstrata.py

--- Python_Studies/Modulo2Python/semana5/aula03/classe_abstrata.py
+++ Python_Studies/Modulo2Python/semana5/aula03/classe_abstrata.py
@@ -1,7 +1,31 @@
 from abc import ABC, abstractmethod
 
+
 class Pagamento(ABC):
 
     @abstractmethod
     def pagar(self):
-        pass
+        print("Pagamento ...")
+
+
+class PagamentoCartao(Pagamento):
+
+    def pagar():
+        print("Pagamento Cartão...")
+
+
+class PagamentoBoleto(Pagamento):
+
+    def pagar():
+        print("Pagamento Boleto...")
+
+
+class PagamentoPix(Pagamento):
+
+    def pagar():
+        print("Pagamento Pix...")
+
+
+PagamentoBoleto.pagar()
+PagamentoCartao.pagar()
+PagamentoPix.pagar()
