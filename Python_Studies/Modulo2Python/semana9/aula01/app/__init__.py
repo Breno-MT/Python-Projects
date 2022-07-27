97ea30f708f6fef1e8001880cae57db58a6c0665
Breno <hentgenp@gmail.com>
2022-07-26 21:59:24 -0300

    Update Flask

--- Python_Studies/Modulo2Python/semana9/aula01/app/__init__.py
+++ Python_Studies/Modulo2Python/semana9/aula01/app/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
+
+from app.config import app_config
 
 app = Flask(__name__)
+
+
+app.config.from_object(app_config[os.getenv('FLASK_ENV')])
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_world(): return jsonify({"teste": "teste"})
