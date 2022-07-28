36f848879775b3f8b1359dcd54e518198d5d146c
Breno <hentgenp@gmail.com>
2022-07-27 22:17:43 -0300

    Update app/controllers/technologies.py

--- Python_Studies/Modulo2Python/semana9/aula01/src/app/controllers/technologies.py
+++ Python_Studies/Modulo2Python/semana9/aula01/src/app/controllers/technologies.py
@@ -1,6 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 technology = Blueprint('technology', __name__, url_prefix='/technology')
 
+lista_json = {"data": ["JavaScript", "Python", "C++"]}
+
+
 @technology.route("/", methods = ["GET"])
-def list_all_technologies(): return {"data": ["Javascript", "Python", "C++"]}
+def list_all_technologies(): return lista_json
+
+@technology.route("/", methods = ["POST"])
+def add_new_technologies(): 
+    
+    data = request.get_json()
+    print(data)
+    
+    lista_json["data"].append(data['data'])
+
+    return lista_json
