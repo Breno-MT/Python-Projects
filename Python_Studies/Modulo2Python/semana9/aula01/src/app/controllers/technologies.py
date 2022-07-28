e4dbef617b01ee2c438a495662407336ecdad4c2
Breno <hentgenp@gmail.com>
2022-07-27 21:44:53 -0300

    Update aula01/src/app/controllers/technologies.py

--- Python_Studies/Modulo2Python/semana9/aula01/src/app/controllers/technologies.py
+++ Python_Studies/Modulo2Python/semana9/aula01/src/app/controllers/technologies.py
@@ -2,3 +2,5 @@ from flask import Blueprint
 
 technology = Blueprint('technology', __name__, url_prefix='/technology')
 
+@technology.route("/", methods = ["GET"])
+def list_all_technologies(): return {"data": ["Javascript", "Python", "C++"]}
